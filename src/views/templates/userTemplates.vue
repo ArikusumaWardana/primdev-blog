872fc8f986c99af8ac6c38893155dcb8dea09f8d
Arikusuma Wardana <87961962+ArikusumaWardana@users.noreply.github.com>
2024-06-22 10:41:48 +0800

    Add navbar

--- src/views/templates/userTemplates.vue
+++ src/views/templates/userTemplates.vue
@@ -1,5 +1,10 @@
+<script>
+import Header from '@/components/layout/header.vue'
+</script>
+
 <template>
   <!-- todo show header -->
+   <Header />
   <!-- todo show homepage -->
   <h1 class="font-bold text-5xl flex items-center justify-center h-screen">Hei There User</h1>
   <!-- todo show footer -->
