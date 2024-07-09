2c7cc80a81cc6caa12003eb6f651dbfb2a2f3aa0
arikusumawardana09 <arikusumawardana09@gmail.com>
2024-07-09 02:47:02 +0000

    Membuat component card

---
+++ src/components/card.vue
@@ -0,0 +1,22 @@
+<script>
+    import { useRoute } from 'vue-router';
+    import { truncateText } from '@/helper/globalVariable'
+
+    const props = defineProps({
+        blog_id: Number,
+        title: String,
+        slug: String,
+        image: String,
+        content: String,
+    })
+</script>
+
+<template>
+    <router-link class="flex dark:bg-gray-800 bg-gray-100 p-4 rounded-lg shadow-md">
+        <img :src="props.image" :alt="props.title" class="w-1/3 h-full object-cover rounded-lg mr-4">
+        <div class="w-2/3">
+            <h3 class="text-xl font-semibold mb-2">{{ truncateText(props.title, 18) }}</h3>
+            <p class="text-gray-500 dark:text-gray-400">{{ truncateText(props.content, 80) }}</p>
+        </div>
+    </router-link>
+</template>
