4dd10a77a7748b22f3bf5b36a1352fbf14a08ec8
arikusumawardana09 <arikusumawardana09@gmail.com>
2024-07-18 11:11:41 +0000

    add new global variable

--- src/helper/globalVariable.js
+++ src/helper/globalVariable.js
@@ -5,4 +5,10 @@ export const truncateText = (text, length) => {
     // Otherwise, truncate the text to the desired length and add an ellipsis
     return text.slice(0, length) + '...';
   }
+
+// Variable for get the API 
+export const BASE_URL = 'https://primdev.alwaysdata.net/api/'
+
+// Variable for get the token from local storage
+export const token = localStorage.getItem('token')
   
