3dce2b4d0e4631ba49538a703343423b78384fd8
Arikusuma Wardana <87961962+ArikusumaWardana@users.noreply.github.com>
2024-07-18 22:13:17 +0800

    fixing error login function

--- src/views/pages/auth/register.vue
+++ src/views/pages/auth/register.vue
@@ -1,23 +1,25 @@
 <script setup>
-  import baseInput from '@/components/baseInput.vue';
-  import {reactive, ref} from 'vue';
-  import axios from 'axios';
-  import { BASE_URL } from '@/helper/globalVariable';
-  import router from '@/router/routes';
+import BaseInput from '@/components/baseInput.vue';
+import { reactive, ref } from 'vue';
+import axios from 'axios'
+import { BASE_URL } from '@/helper/globalVariable'
+import router from '@/router/routes'
 
-  const registerForm = reactive({
-    username: '',
+  const inputData = reactive({
+    name: '',
     email: '',
     password: '',
-    repeatPassword: '',
+    confirm_password: ''
   });
 
-  // const isLoading = ref(false)
 
-  const register = async () => {
-  if (registerForm.repeatPassword == registerForm.password) {
+  const isLoading = ref(false)
+
+const register = async () => {
+  if (inputData.confirm_password == inputData.password) {
+    isLoading.value = true
     try {
-      const response = await axios.post('https://primdev.alwaysdata.net/api/register', registerForm)
+      const response = await axios.post(BASE_URL + 'register', inputData);
       if (response.status == 200) {
         localStorage.setItem('token', response.data.token)
         router.push('/auth/login')
@@ -25,11 +27,14 @@
       }
     } catch (error) {
       console.log(error)
+    } finally {
+      isLoading.value = false
     }
   } else {
     console.log('Confirm Password dan Password tidak sama')
   }
 }
+
 </script>
 
 <template>
@@ -37,10 +42,38 @@
       <div class="bg-gray-100 dark:bg-gray-800 p-8 rounded-lg shadow-2xl w-full max-w-sm mx-4">
         <h2 class="text-2xl font-bold text-gray-800 mb-6 text-center dark:text-white">Register</h2>
         <form @submit.prevent="register" method="post" class="text-sm">
-          <baseInput v-model="registerForm.username" input-title="Username" input-name="username" input-placeholder="Type your username" input-icon="fas fa-user"/>
-          <baseInput v-model="registerForm.email" input-title="Email" input-name="email" input-placeholder="Type your email" input-icon="fas fa-envelope" input-type="email"/>
-          <baseInput v-model="registerForm.password" input-title="Password" input-name="password" input-placeholder="Type your password" input-icon="fas fa-lock" input-type="password"/>
-          <baseInput v-model="registerForm.repeatPassword" input-title="Repeat Password" input-name="repeatPassword" input-placeholder="Repeat your password" input-icon="fas fa-repeat" input-type="password"/>
+          <baseInput 
+          v-model="inputData.name" 
+          input-title="Name" 
+          input-name="name" 
+          input-placeholder="Type your name" 
+          input-icon="fas fa-user"
+          input-type="text"/>
+
+          <baseInput 
+          v-model="inputData.email" 
+          input-title="Email" 
+          input-name="email" 
+          input-placeholder="Type your email" 
+          input-icon="fas fa-envelope" 
+          input-type="email"/>
+
+          <baseInput 
+          v-model="inputData.password" 
+          input-title="Password" 
+          input-name="password" 
+          input-placeholder="Type your password" 
+          input-icon="fas fa-lock" 
+          input-type="password"/>
+
+          <baseInput 
+          v-model="inputData.confirm_password" 
+          input-title="Confirm Password" 
+          input-name="confirm_password" 
+          input-placeholder="Confirm your password" 
+          input-icon="fas fa-repeat" 
+          input-type="password"/>
+          
           <div class="flex items-center justify-between mb-6">
             <button type="submit" class="w-full py-2 px-4 rounded-lg bg-gradient-to-r from-blue-500 to-blue-700 hover:from-blue-600 hover:to-blue-800 text-white font-bold focus:outline-none focus:ring focus:ring-blue-500 uppercase">
               Register
