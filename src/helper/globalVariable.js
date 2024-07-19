77fcd8ac329057d25bc21d75cd97780c45635574
Arikusuma Wardana <87961962+ArikusumaWardana@users.noreply.github.com>
2024-07-19 18:08:54 +0800

    update fitur truncate text

--- src/helper/globalVariable.js
+++ src/helper/globalVariable.js
@@ -1,10 +1,8 @@
-// Function to truncate text to a specified length
-export const truncateText = (text, length) => {
-    // If the text is shorter than or equal to the desired length, return the original text
-    if (text.length <= length) return text;
-    // Otherwise, truncate the text to the desired length and add an ellipsis
-    return text.slice(0, length) + '...';
-  }
+// Function to truncate text
+export function truncateText(text, length) {
+  return text.length > length ? text.substring(0, length) + '...' : text;
+}
+
 
 // Variable for get the API 
 export const BASE_URL = 'https://primdev.alwaysdata.net/api/'
